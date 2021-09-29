63abeee16e56d1904c2d19679447c1d139d871ac
Heidi Cao <heidihcao@gmail.com>
2021-09-29 11:51:15 -0700

    This should be the final draft.

--- js/app.js
+++ js/app.js
@@ -47,9 +47,11 @@ if(answer4 === 'yes' || answer4 === 'y'){
 
 let answer5=prompt("Am I enjoying Code Fellows 201?").toLowerCase();
 if(answer5 === 'yes' || answer5 === 'y'){
-  alert('You bet!');
+  alert('You bet!'+ 'This is the last question, '+username+ ", and you nailed it!");
   /*console.log('You are right - I like this class!');*/
 } else{
-  alert('Try again :c');
+  alert('This is the last question, '+username+'Try again :c');
   /*console.log ('Oops. Can you try again?');*/
 }
+
+alert ("Thank you for playing the guessing game, "+username+"! you did a great job!");
